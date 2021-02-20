c9eefa978a960f44f5b985e41cc2df6279e7b34e
Arte <jiangtao.cheng@orderplus.com>
2021-02-20 17:20:32 +0800

    修改数据库地址

--- auth-center/src/test/java/com/artech/auth/AuthCenterApplicationTests.java
+++ auth-center/src/test/java/com/artech/auth/AuthCenterApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AuthCenterAuthCenterApplicationTests {
+public class AuthCenterApplicationTests {
 
 	@Autowired
 	private UserService userService;
